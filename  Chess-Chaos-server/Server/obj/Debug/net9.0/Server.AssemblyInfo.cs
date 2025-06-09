c280b45b50ad99efecccf3f295bf012d43c88de0
Sean-mn <b01080080329@gmail.com>
2025-06-09 20:35:00 +0900

    feat: AuthController 생성
    #1

---  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a8d383d3b1d3c2c255898bd32b398b985a8c01c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a53aa8ea51f9966cc49b927b0bd027839015b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

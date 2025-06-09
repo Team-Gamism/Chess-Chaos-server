df82b1156fc896dd065381f33f126eff4dd1d3e0
Sean-mn <b01080080329@gmail.com>
2025-06-09 20:57:32 +0900

    feat: MySQL 관련 패키지 설치
    Pomelo.EntityFrameworkCore.MySql 패키지 설치
    
    #1

---  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a53aa8ea51f9966cc49b927b0bd027839015b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42dc9b1f8857c40f9db4501a3ab8a28dcfdfde5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

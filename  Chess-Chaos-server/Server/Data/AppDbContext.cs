e3e007bfa10ee8b47553426a2a021d2010c999e7
Sean-mn <b01080080329@gmail.com>
2025-06-09 17:43:23 +0900

    feat: AppDbContext 구현
    AppDbContext 구현
    PlayerLoginData DbSet 생성
    
    #1

---
+++  Chess-Chaos-server/Server/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Model.Entity;
+
+namespace Server.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<PlayerLoginData> PlayerLoginDatas { get; set; } = null!;
+}

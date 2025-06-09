c3adb6135cdf7914ec7261655bd2ebffdc15aac6
Sean-mn <b01080080329@gmail.com>
2025-06-09 18:00:36 +0900

    feat: 비밀번호 관련 서비스 구현
    IPasswordService및 IPasswordService를 구현한 PasswordService 구현
    
    #1

---
+++  Chess-Chaos-server/Server/Service/Interface/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Service.Interface;
+
+public interface IPasswordService
+{
+    string HashPassword(string password);
+    bool VerifyHashPassword(string password, string hashedPassword);
+}

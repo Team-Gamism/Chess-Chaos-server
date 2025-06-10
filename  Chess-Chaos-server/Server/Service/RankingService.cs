5427b79b49fdf9b35dfaf1d3a7eef517f1e5a6b6
Sean-mn <b01080080329@gmail.com>
2025-06-10 18:25:09 +0900

    feat: RankingService 구현
    IRankingService 및 RankingService 구현
    
    #5

---
+++  Chess-Chaos-server/Server/Service/RankingService.cs
@@ -0,0 +1,48 @@
+﻿using Server.Model.Ranking.Entity;
+using Server.Repository.Interface;
+using Server.Service.Interface;
+
+namespace Server.Service;
+
+public class RankingService : IRankingService
+{
+    private readonly IRankingRepository _repository;
+
+    public RankingService(IRankingRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task SubmitRankingAsync(string playerId, int score)
+    {
+        var exists = await _repository.GetByPlayerIdAsync(playerId);
+
+        if (exists == null)
+        {
+            var newRanking = new PlayerRankingData
+            {
+                PlayerId = playerId,
+                Score = score,
+            };
+            
+            await _repository.AddAsync(newRanking);
+        }
+        else if (score > exists.Score)
+        {
+            exists.Score = score;
+            await _repository.UpdateAsync(exists);
+        }
+        
+        await _repository.SaveAsync();
+    }
+
+    public async Task<List<PlayerRankingData>> GetTopRankingsAsync(int count)
+    {
+        return await _repository.GetTopAsync(count);
+    }
+
+    public async Task<PlayerRankingData?> GetPlayerRankingAsync(string playerId)
+    {
+        return await _repository.GetByPlayerIdAsync(playerId);
+    }
+}

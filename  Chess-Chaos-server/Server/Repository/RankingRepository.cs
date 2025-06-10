3616c55f5ea1c5668730a23bc284c8c3641fd8dd
Sean-mn <b01080080329@gmail.com>
2025-06-10 18:08:51 +0900

    feat: RankingRepository 구현
    IRankingRepository 및 RankingRepository 구현
    
    #5

---
+++  Chess-Chaos-server/Server/Repository/RankingRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Model.Ranking.Entity;
+using Server.Repository.Interface;
+
+namespace Server.Repository;
+
+public class RankingRepository : IRankingRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public RankingRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<PlayerRankingData?> GetByPlayerIdAsync(string playerId)
+    {
+        return await _dbContext.PlayerRankings
+            .SingleOrDefaultAsync(p => p.PlayerId == playerId);
+    }
+
+    public async Task<List<PlayerRankingData>> GetTopAsync(int count)
+    {
+        return await _dbContext.PlayerRankings
+            .OrderByDescending(p => p.Score)
+            .Take(count)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(PlayerRankingData data)
+    {
+        await _dbContext.PlayerRankings.AddAsync(data);
+    }
+
+    public async Task UpdateAsync(PlayerRankingData data)
+    {
+        _dbContext.PlayerRankings.Update(data);
+        await Task.CompletedTask;
+    }
+
+    public async Task SaveAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}

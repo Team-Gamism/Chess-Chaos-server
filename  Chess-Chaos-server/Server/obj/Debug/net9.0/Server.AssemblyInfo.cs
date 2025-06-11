e07278ef3768e11dfefaf44ea674f7c9b8cc857e
Sean-mn <b01080080329@gmail.com>
2025-06-11 13:59:14 +0900

    feat: Dapper 패키지 설치
    Dapper 패키지 설치
    
    #7

---  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2494dd8b906e312d97ddab5e9624186d372c6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b88aceda3772822b5cc53aae1bbf068696529a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

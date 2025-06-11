d9451dfc50c6898f1f4e5039c4a6136e367031dc
Sean-mn <b01080080329@gmail.com>
2025-06-12 08:51:23 +0900

    feat: CacheService 구현
    ICacheService 및 CacheService 구현
    
    #9

---
+++  Chess-Chaos-server/Server/Service/CacheService.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+using Server.Service.Interface;
+
+namespace Server.Service;
+
+public class CacheService : ICacheService
+{
+    private readonly IDistributedCache _cache;
+
+    public CacheService(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+    
+    public async Task<T?> GetDataAsync<T>(string key)
+    {
+        var data = await _cache.GetStringAsync(key);
+        return data is null ? default : JsonSerializer.Deserialize<T>(data);
+    }
+
+    public async Task SetDataAsync<T>(string key, T value, TimeSpan? expiredTime = null)
+    {
+        var options = new DistributedCacheEntryOptions();
+        if (expiredTime.HasValue)
+            options.AbsoluteExpirationRelativeToNow = expiredTime.Value;
+        
+        var data = JsonSerializer.Serialize(value);
+        await _cache.SetStringAsync(key, data, options);
+    }
+
+    public async Task RemoveDataAsync(string key)
+    {
+        await _cache.RemoveAsync(key);
+    }
+}

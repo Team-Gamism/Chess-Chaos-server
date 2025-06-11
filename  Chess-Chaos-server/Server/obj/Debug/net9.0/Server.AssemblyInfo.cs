9bfab4f04616d07c1aee01139ed530b00f6bb5f5
Sean-mn <b01080080329@gmail.com>
2025-06-11 14:20:14 +0900

    modify: 서버 설정 변경

---  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++  Chess-Chaos-server/Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e07278ef3768e11dfefaf44ea674f7c9b8cc857e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32758d5ff0fa0c81b6f4c9548331e5389d26ec86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

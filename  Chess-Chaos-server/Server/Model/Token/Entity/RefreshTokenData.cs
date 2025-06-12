7e00a48e17e412cc392522db6d4e0652bd4bf7ff
Sean-mn <b01080080329@gmail.com>
2025-06-12 20:04:45 +0900

    feat: refreshToken 엔티티 생성
    #12

---
+++  Chess-Chaos-server/Server/Model/Token/Entity/RefreshTokenData.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Model.Token.Entity;
+
+[Table("refresh_token_data")]
+public class RefreshTokenData
+{
+    [Key]
+    [Column("id")]
+    public string Id { get; set; } = null!;
+    
+    [Required]
+    [Column("player_id")]
+    [MaxLength(15)]
+    public string PlayerId { get; set; } = null!;
+    
+    [Required]
+    [Column("refresh_token")]
+    [MaxLength(255)]
+    public string RefreshToken { get; set; } = null!;
+    
+    [Required]
+    [Column("expiry_date")]
+    public DateTime ExpiryDate { get; set; }
+}

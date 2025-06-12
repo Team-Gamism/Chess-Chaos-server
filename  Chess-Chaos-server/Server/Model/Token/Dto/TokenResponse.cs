8bb176a9cfec24d9918e4f025533df2b961df0d6
Sean-mn <b01080080329@gmail.com>
2025-06-12 19:53:48 +0900

    feat: TokenResponse Dto 생성
    #12

---
+++  Chess-Chaos-server/Server/Model/Token/Dto/TokenResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Model.Token.Dto;
+
+public class TokenResponse
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+}
